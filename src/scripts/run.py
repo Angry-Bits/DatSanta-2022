706c499652eee3b954a006f44141f3a2db159f19
Igor Gakhov <gakhov_igor@rambler.ru>
2022-12-23 19:29:12 +0300

    Separate the logic of getting json

--- src/scripts/run.py
+++ src/scripts/run.py
@@ -5,7 +5,7 @@ import requests
 
 
 SRC = Path.cwd()
-MAP_FIXTURE_NAME = 'map.json'
+MAP_FIXTURE_NAME = 'map2.json'
 MAP_FIXTURE_PATH = SRC.joinpath(MAP_FIXTURE_NAME)
 
 MAP_ID = 'faf7ef78-41b3-4a36-8423-688a61929c08'
@@ -15,6 +15,10 @@ URL_MAP_GET = 'https://datsanta.dats.team/json/map/{}.json'
 def get_map(id=MAP_ID):
     r = requests.get(URL_MAP_GET.format(id))
     data = json.loads(r.text)
+    return data
+
+
+def visualize_json(data):
     data = json.dumps(data, indent=4)
     return data
 
@@ -24,4 +28,7 @@ def write_to_file(data, file_path=SRC):
         file.write(data)
 
 
-write_to_file(get_map(), MAP_FIXTURE_PATH)
+if __name__ == '__main__':
+    json_data = get_map()
+    visualized = visualize_json(json_data)
+    write_to_file(visualized, MAP_FIXTURE_PATH)
